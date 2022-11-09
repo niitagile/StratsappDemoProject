85ef27bb5126f677645066f234ffb50be0f3cec4
niitagile <nidhiguptatech@gmail.com>
2022-11-09 10:51:06 +0530

    DisplayMessage Commit2

--- SecuritySystem/src/DisplayMessage.java
+++ SecuritySystem/src/DisplayMessage.java
@@ -4,6 +4,7 @@ public class DisplayMessage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		System.out.println("Hello All stratsapp guys");
 	}
 
 }
